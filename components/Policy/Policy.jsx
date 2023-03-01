b46b574fc3de4fee302799b53a13fd4c7d9056f1
O-Isaac <zaragozamendozaisaac@gmail.com>
2023-03-01 21:39:34 +0100

    fix(Policy): Fix class to className
    Change class to className in file `Component/Policy/Policy.jsx` to fix problem on webpack.

--- components/Policy/Policy.jsx
+++ components/Policy/Policy.jsx
@@ -2,15 +2,15 @@ export default function Policy({ title, description, href }) {
 
   return (
     <a
-      class="relative flex items-start justify-between rounded-xl border dark:bg-slate-100 border-gray-100 p-4 shadow-xl sm:p-6 lg:p-8"
+      className="relative flex items-start justify-between rounded-xl border dark:bg-slate-100 border-gray-100 p-4 shadow-xl sm:p-6 lg:p-8"
       href={href}
     >
-      <div class=" text-gray-500">
-        <h3 class="mt-2 text-lg font-bold text-gray-900 sm:text-xl">
+      <div className=" text-gray-500">
+        <h3 className="mt-2 text-lg font-bold text-gray-900 sm:text-xl">
           {title}
         </h3>
 
-        <p class="mt-2 hidden text-sm sm:block">
+        <p className="mt-2 hidden text-sm sm:block">
           {description}
         </p>
       </div>
