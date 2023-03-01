8e3187cfc82dc662a36ec30d426814f5061f127f
O-Isaac <zaragozamendozaisaac@gmail.com>
2023-03-01 23:58:21 +0100

    feat(policy): remake buttons policy!

--- components/Policy/Policy.jsx
+++ components/Policy/Policy.jsx
@@ -1,19 +1,55 @@
-export default function Policy({ title, description, href }) {
+import { useEffect } from "react";
+import styles from "./Policy.module.css";
+
+export default function Card() {
+  const { card, cards, cardContent, cardBorder } = styles;
+
+  useEffect(() => {
+    document.getElementById("cards").onmousemove = (e) => {
+      for (const card of document.getElementsByClassName("card")) {
+        const rect = card.getBoundingClientRect(),
+          x = e.clientX - rect.left,
+          y = e.clientY - rect.top;
+
+        card.style.setProperty("--mouse-x", `${x}px`);
+        card.style.setProperty("--mouse-y", `${y}px`);
+      }
+    };
+  });
+
+  /**
+   * <Policy href="https://drive.google.com/file/d/1gsylAEfdtnDa-W3Y5Q8MITJPt_dpeKKD/view" title="Privacy Policy" description="This Privacy Policy establishes the terms in which Fate/Grand Order (Spanish Project) uses or
+does not use the information that is provided by its users when using its website and how it
+does not collect or store any data from the App that we provide to patch FGO." />
+   */
 
   return (
-    <a
-      className="relative flex items-start justify-between rounded-xl border dark:bg-slate-100 border-gray-100 p-4 shadow-xl sm:p-6 lg:p-8"
-      href={href}
-    >
-      <div className=" text-gray-500">
-        <h3 className="mt-2 text-lg font-bold text-gray-900 sm:text-xl">
-          {title}
-        </h3>
+    <div id="cards" className={cards}>
+      <a href="https://drive.google.com/file/d/1-hFZ6As-YTuGxxWQKKl-jo43UYzd6y51/view" target="_blank">
+        <div className={card + " card"}>
+          <div className={cardBorder}></div>
+          <div className={cardContent}>
+            <div className="text-center">
+              <h3 className="text-4xl font-bold leading-normal">
+                Politica de privacidad
+              </h3>
+              <h4>Click para leer</h4>
+            </div>
+          </div>
+        </div>
+      </a>
 
-        <p className="mt-2 hidden text-sm sm:block">
-          {description}
-        </p>
-      </div>
-    </a>
+      <a href="https://drive.google.com/file/d/1gsylAEfdtnDa-W3Y5Q8MITJPt_dpeKKD/view" target="_blank">
+        <div className={card + " card"}>
+            <div className={cardBorder}></div>
+            <div className={cardContent}>
+            <div className="text-center">
+                <h3 className="text-4xl font-bold leading-normal">Privacy Policy</h3>
+                <h4>Click here to read!</h4>
+            </div>
+            </div>
+        </div>
+      </a>
+    </div>
   );
 }
