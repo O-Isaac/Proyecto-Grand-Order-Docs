744f5191b67d52572def31296054d1ab8b158fc8
Isaac <98283822+O-Isaac@users.noreply.github.com>
2023-03-04 10:18:59 +0100

    feat(config): update config file
    Actualización del repositorio original

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.jsx',
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
-    })
-
-    return config
-  }
+  themeConfig: './theme.config.jsx'
 })
 
 module.exports = withNextra({
