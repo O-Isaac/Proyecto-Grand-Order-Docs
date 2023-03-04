a66fa3169e5d0709c6aebb69a58c007cacdf544c
O-Isaac <zaragozamendozaisaac@gmail.com>
2023-03-04 18:55:36 +0100

    feat(og): add og image generator & meta image

--- theme.config.jsx
+++ theme.config.jsx
@@ -2,7 +2,8 @@ import { useRouter } from "next/router";
 import { useConfig } from "nextra-theme-docs";
 
 export default {
-  docsRepositoryBase: 'https://github.com/O-Isaac/Proyecto-Grand-Order-Docs/tree/master',
+  docsRepositoryBase:
+    "https://github.com/O-Isaac/Proyecto-Grand-Order-Docs/tree/master",
   logo: (
     <>
       <img className="logo" width="40px" src="/images/bg-logo.svg" alt="Logo" />
@@ -22,9 +23,13 @@ export default {
   head: () => {
     const { asPath, defaultLocale, locale } = useRouter();
     const { frontMatter } = useConfig();
-    const url =
-      "https://proyectograndorder.es" +
-      (defaultLocale === locale ? asPath : `/${locale}${asPath}`);
+    const hostname =
+      process.env.VERCEL_ENV === "production"
+        ? "proyectograndorder.es"
+        : process.env.VERCEL_URL || "proyectograndorder.es";
+
+    const path = defaultLocale === locale ? asPath : `/${locale}${asPath}`;
+    const url = "https://" + hostname + path;
 
     return (
       <>
@@ -39,22 +44,90 @@ export default {
             frontMatter.description || "¡Juega Fate Grand Order en español!"
           }
         />
-        <link rel="apple-touch-icon" sizes="57x57" href="/favicon/apple-icon-57x57.png" />
-        <link rel="apple-touch-icon" sizes="60x60" href="/favicon/apple-icon-60x60.png" />
-        <link rel="apple-touch-icon" sizes="72x72" href="/favicon/apple-icon-72x72.png" />
-        <link rel="apple-touch-icon" sizes="76x76" href="/favicon/apple-icon-76x76.png" />
-        <link rel="apple-touch-icon" sizes="114x114" href="/favicon/apple-icon-114x114.png" />
-        <link rel="apple-touch-icon" sizes="120x120" href="/favicon/apple-icon-120x120.png" />
-        <link rel="apple-touch-icon" sizes="144x144" href="/favicon/apple-icon-144x144.png" />
-        <link rel="apple-touch-icon" sizes="152x152" href="/favicon/apple-icon-152x152.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-icon-180x180.png" />
-        <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="96x96" href="/favicon/favicon-96x96.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <meta
+          property="og:image"
+          content={
+            frontMatter.title
+              ? `${"https://" + hostname}/api/og?title=${frontMatter.title}`
+              : `${"https://" + hostname}/api/og
+          `
+          }
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="57x57"
+          href="/favicon/apple-icon-57x57.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="60x60"
+          href="/favicon/apple-icon-60x60.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="72x72"
+          href="/favicon/apple-icon-72x72.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href="/favicon/apple-icon-76x76.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="114x114"
+          href="/favicon/apple-icon-114x114.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="120x120"
+          href="/favicon/apple-icon-120x120.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="144x144"
+          href="/favicon/apple-icon-144x144.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/favicon/apple-icon-152x152.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-icon-180x180.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/android-icon-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href="/favicon/favicon-96x96.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
         <link rel="manifest" href="/favicon/manifest.json" />
         <meta name="msapplication-TileColor" content="#ffffff" />
-        <meta name="msapplication-TileImage" content="/favicon/ms-icon-144x144.png" />
+        <meta
+          name="msapplication-TileImage"
+          content="/favicon/ms-icon-144x144.png"
+        />
         <meta name="theme-color" content="#ffffff" />
       </>
     );
