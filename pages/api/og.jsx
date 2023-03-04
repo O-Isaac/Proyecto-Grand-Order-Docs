0a9c4c15ba0597e65bffdc4c43ea7356a1f3ebc0
O-Isaac <zaragozamendozaisaac@gmail.com>
2023-03-04 18:55:36 +0100

    feat(og): add og image generator & meta image

--- pages/api/og.jsx
+++ pages/api/og.jsx
@@ -4,49 +4,79 @@ export const config = {
   runtime: "edge",
 };
 
-/**
- * 
- * @param {import("next/server").NextRequest} req 
- * @returns 
- */
-export default function (req) {
-  const searchParams = new URLSearchParams(req.nextUrl.search);
-  const text = searchParams.get("text")
+export default function handler(req) {
+  try {
+    const { searchParams } = new URL(req.url);
+    const hasTitle = searchParams.has("title");
+    const getRandom = (arr) => arr[Math.floor(Math.random() * arr.length)];
+    const isLight = getRandom(["dark", "light"]) === "light";
 
-  return new ImageResponse(
-    (
-      <div
-        style={{
-          height: "100%",
-          width: "100%",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "flex-start",
-          justifyContent: "center",
-          backgroundColor: "#fff",
-          fontSize: 40,
-          letterSpacing: -2,
-          fontWeight: 800,
-          backgroundImage: "url(https://i.imgur.com/CijJGgX.png)",
-        }}
-      >
-        <div
-          style={{
-            marginTop: -40,
-            marginLeft: 60,
-            backgroundImage: 'linear-gradient(90deg, rgb(0, 124, 240), rgb(0, 223, 216))',
-            backgroundClip: 'text',
-            '-webkit-background-clip': 'text',
-            color: 'transparent',
-          }}
-        >
-          {text ?? "Acerca del proyecto"}
-        </div>
-      </div>
-    ),
-    {
-      width: 1200,
-      height: 600,
+    const options = {
+      width: 1020,
+      height: 170,
+    };
+
+    if (isLight) {
+      return new ImageResponse(
+        (
+          <div
+            style={{ backgroundColor: "transparent" }}
+            tw="flex flex-col justify-center items-center w-full h-full"
+          >
+            <div tw="flex bg-slate-100 w-[1000px] h-[150px] rounded-[10px] border border-slate-300 items-center p-8 overflow-hidden">
+              <div tw="flex flex-col justify-center">
+                <span tw="text-2xl">Proyecto Grand Order</span>
+                <span tw="text-xl text-gray-700">
+                  {hasTitle ? searchParams.get("title") : "¡Juega Fate/Grand Order en español!"}
+                </span>
+              </div>
+              <div tw="flex">
+                <img
+                  tw="mt-40"
+                  src={`https://proyectograndorder.es/themes/light/char.png`}
+                />
+              </div>
+            </div>
+          </div>
+        ),
+        options
+      );
     }
-  );
+    
+    return new ImageResponse(
+      (
+        <div
+          style={{ backgroundColor: "transparent" }}
+          tw="flex flex-col justify-center items-center w-full h-full"
+        >
+          <div
+            style={{
+              backgroundColor: "rgb(20,20,20)",
+              border: "1px solid rgba(255,255,255,0.6)",
+            }}
+            tw="flex w-[1000px] h-[150px] rounded-[10px] items-center p-8 overflow-hidden"
+          >
+            <div tw="flex flex-col justify-center">
+              <span tw="text-2xl text-white">Proyecto Grand Order</span>
+              <span tw="text-xl text-gray-300">
+                {hasTitle ? searchParams.get("title") : "¡Juega Fate/Grand Order en español!"}
+              </span>
+            </div>
+            <div tw="flex">
+              <img
+                tw="mt-40"
+                src={`https://proyectograndorder.es/themes/dark/char.png`}
+              />
+            </div>
+          </div>
+        </div>
+      ),
+      options
+    );
+  } catch (error) {
+    return new Response(`Failed to generate the image`, {
+      status: 500,
+      error: error.message,
+    });
+  }
 }
