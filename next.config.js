f1ae0437a7b26467b0b728a249d3c92225a5653d
O-Isaac <zaragozamendozaisaac@gmail.com>
2023-03-04 01:07:28 +0100

    fix(next): remove webpack config
    webpack config bad implementation.

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.jsx',
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
-    })
-
-    return config
-  }
 })
 
 module.exports = withNextra()
