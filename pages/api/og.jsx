389592e3a21658ec3840b2d22a80282210c8d9c9
O-Isaac <zaragozamendozaisaac@gmail.com>
2023-03-04 19:06:56 +0100

    feat(meta): change image size

--- pages/api/og.jsx
+++ pages/api/og.jsx
@@ -12,8 +12,8 @@ export default function handler(req) {
     const isLight = getRandom(["dark", "light"]) === "light";
 
     const options = {
-      width: 1190,
-      height: 340,
+      width: 1400,
+      height: 700,
     };
 
     if (isLight) {
