18c1fe06e9f889d057642db070f6e71436a02491
O-Isaac <zaragozamendozaisaac@gmail.com>
2023-03-05 12:34:34 +0100

    feat(Hero): change backgroundUrl to image
    Increase performance website

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -2,30 +2,29 @@ import { useEffect, useState } from "react";
 import { useTheme } from "nextra-theme-docs";
 import Link from "next/link";
 import Image from "next/image";
-import style from "./Hero.module.css"
+import style from "./Hero.module.css";
 
 export default function Hero() {
   const { resolvedTheme } = useTheme();
   const [theme, setTheme] = useState("dark");
-  const [key, setKey] = useState(0)
-
-  const stylesBackground = {
-    backgroundImage: `url(/themes/${theme}/hero_background_up.jpg)`,
-    backgroundSize: "cover",
-    backgroundPosition: "center"
-  };
+  const [key, setKey] = useState(0);
 
   useEffect(() => {
     if (resolvedTheme) {
       setTheme(resolvedTheme);
-      setKey((prevKey) => ++prevKey)
+      setKey((prevKey) => ++prevKey);
     }
   }, [resolvedTheme]);
 
   return (
-    <div key={key} style={stylesBackground} className={"grid place-items-center h-[800px] " + style.heroWrapper}>
+    <div key={key} className={"grid place-items-center h-[800px] relative " + style.heroWrapper}>
       <Link className="h-full" href="/instalar">
         <section className="h-full w-auto grid place-items-center">
+          <Image
+            className="object-cover"
+            src={`/themes/${theme}/hero_background_up.jpg`}
+            fill
+          />
           <Image
             className={"h-full object-cover " + style.chrImg}
             width={907}
