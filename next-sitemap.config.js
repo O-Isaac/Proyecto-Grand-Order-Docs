f9d8486acecef82a5ba2eec338fa2a0e190dda83
O-Isaac <zaragozamendozaisaac@gmail.com>
2023-03-05 12:09:55 +0100

    feat(sitemap): change hostname on production

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.VERCEL_URL
+  siteUrl: process.env.VERCEL_URL && process.env.VERCEL_ENV !== "production"
     ? `https://${process.env.VERCEL_URL}`
     : "https://proyectograndorder.es",
   generateRobotsTxt: true,
