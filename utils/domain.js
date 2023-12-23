6d3c745ef475c296500a9e39e77fb5df4fe1aef8
O-Isaac <zaragozamendozaisaac@gmail.com>
2023-12-23 20:13:08 +0100

    perf(og): Link with external services feat(packages): Remove @vercel/og feat(api/og): Remove route internal service

--- utils/domain.js
+++ utils/domain.js
@@ -9,11 +9,3 @@ export function getHostname () {
 
     return process.env.VERCEL_URL || "proyectograndorder.es"
 }
-
-export function getOgImagePath(hostname, title) {
-    if (title) {
-        return `${"https://" + hostname}/api/og?title=${title}`
-    }
-
-    return `${"https://" + hostname}/api/og`
-}
