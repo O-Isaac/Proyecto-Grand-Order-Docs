955a3b600c5134c6962abb88f165fd34029e59ac
O-Isaac <zaragozamendozaisaac@gmail.com>
2023-12-23 21:26:13 +0100

    feat(blog): Add blog

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -1,51 +1,37 @@
-import { useEffect, useId, useState } from "react";
-import { useTheme } from "nextra-theme-docs";
+import Image from "next/image"
+import Link from "next/link"
+import { useTheme } from "next-themes"
+import { Fragment, useEffect, useState } from "react"
 
-import Link from "next/link";
-import Image from "next/image";
-
-export default function Hero() {
-  const { resolvedTheme } = useTheme();
-  const [bgUrl, setBgUrl] = useState("/themes/light/hero_background_up.jpg");
-  const [charUrl, setCharUrl] = useState("/themes/light/char.webp");
-  const [isLoading, setLoading] = useState(true);
-
-  const bgId = useId();
-  const charId = useId();
+export default function Hero () {
+  const theme = useTheme()
+  const [currentTheme, setCurrentTheme] = useState(null)
 
   useEffect(() => {
-    if (resolvedTheme === undefined || resolvedTheme === "system") return;
-    setBgUrl(`/themes/${resolvedTheme}/hero_background_up.jpg`);
-    setCharUrl(`/themes/${resolvedTheme}/char.webp`);
-    setLoading(false);
-  }, [resolvedTheme]);
-
-  if (isLoading)
-    return (
-      <section className="h-[800px] w-screen relative overflow-hidden"></section>
-    );
+    setCurrentTheme(theme.theme === "system" ? "dark" : theme.theme === "light" ? 'light' : 'dark')
+  }, [theme])
 
   return (
-    <section className="h-[800px] w-screen relative overflow-hidden">
-      <Image
-        key={bgId}
-        className="relative z-10 object-cover object-center"
-        priority
-        src={bgUrl}
-        alt="Image"
-        fill
-      />
+    <section className="relative w-full h-screen">
+      <div className="flex items-center justify-center h-screen mb-4 bg-gray-200 rounded dark:bg-neutral-900">
+        <svg
+          className="w-20 h-20 text-white dark:text-neutral-300 animate-pulse"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 16 20"
+        >
+          <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z" />
+          <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z" />
+        </svg>
+      </div>
 
-      <Link key={charId} href={"/instalar"}>
-        <Image
-          className="relative transition-all hover:opacity-90 active:scale-110 z-10 object-cover w-auto h-full mx-auto"
-          priority
-          width={907}
-          height={764}
-          alt="Instalar"
-          src={charUrl}
-        />
-      </Link>
+      {currentTheme && (
+        <Link  href="/instalar">
+          <Image src={`/themes/${currentTheme}/hero_background_up.jpg`} alt="Background Hero" fill className="z-0 object-cover transition-transform" priority/>
+          <Image src={`/themes/${currentTheme}/char.webp`} alt="Character Hero" fill className="lg:object-contain object-cover transition-transform" priority />
+        </Link>
+      )}
     </section>
-  );
+  )
 }
