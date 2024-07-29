7b7193c1ed15decec1d4a394833fd9edc1e8e702
Isaac <zaragozamendozaisaac@outlook.es>
2024-07-29 19:08:27 +0200

    Change hero url

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -4,7 +4,7 @@ export default function Hero () {
   return (
     <section className="h-screen">
       <figure className="max-h-svh h-svh dark:bg-hero-dark bg-hero-light bg-cover relative parrallax-up-animation overflow-hidden">
-            <Link href="/instalar">
+            <Link href="/special/traum">
                 <picture>
                   <img id='chr' fetchPriority='high' className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-full pt-12 object-cover hover:scale-105 transition-all animate-blurred-fade-in dark:content-chr-dark" loading='eager' src="/themes/light/chr.webp" alt="Servant"/>
                 </picture>
