5b58782e278b4606225077c063c5646af79ae57f
O-Isaac <soporte@proyectograndorder.es>
2024-12-30 22:22:00 +0100

    feat: add support photoswipe

--- components/Image/Image.jsx
+++ components/Image/Image.jsx
@@ -1,7 +1,43 @@
-export default function Image (props) {
-    const { src, alt, height } = props;
-    const imageHeight = height ?? "468px";
-    const imageStyles = { borderRadius: "1rem", height: imageHeight, objectFit: "cover", objectPosition: "center" }
+import "photoswipe/style.css";
+import PhotoSwipeLightbox from "photoswipe/lightbox";
+import PhotoSwipe from "photoswipe";
+import { useEffect } from "react";
 
-    return <img width="832px" height={imageHeight} style={imageStyles} src={src} alt={alt} />
+export default function Image({ src, alt, height, ref }) {
+  const imageHeight = height ?? "468px";
+  const imageStyles = {
+    borderRadius: "1rem",
+    height: imageHeight,
+    objectFit: "cover",
+    objectPosition: "center",
+  };
+
+  useEffect(() => {
+    const lightbox = new PhotoSwipeLightbox({
+      gallery: "#image a",
+      pswpModule: PhotoSwipe,
+    });
+
+    lightbox.init();
+  }, []);
+
+  return (
+    <picture id="image">
+      <a
+        href={src}
+        data-pswp-width="832px"
+        data-pswp-height={imageHeight}
+        target="_blank"
+      >
+        <img
+          ref={ref}
+          width="832px"
+          height={imageHeight}
+          style={imageStyles}
+          src={src}
+          alt={alt}
+        />
+      </a>
+    </picture>
+  );
 }
