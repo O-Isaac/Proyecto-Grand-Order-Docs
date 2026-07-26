64804a219968e79fca207ab213f25599b06ffc74
O-Isaac <zaragozamendozaisaac@outlook.es>
2026-07-26 12:40:56 +0200

    remove cps

--- next.config.js
+++ next.config.js
@@ -5,15 +5,6 @@ const withNextra = require('nextra')({
 
 const isDev = process.env.NODE_ENV !== 'production'
 
-const cspHeader = `
-  default-src 'self';
-  script-src 'self' 'unsafe-inline' ${isDev ? "'unsafe-eval'" : ''} https://pagead2.googlesyndication.com https://googleads.g.doubleclick.net https://www.googletagservices.com https://www.google.com https://www.gstatic.com https://fundingchoicesmessages.google.com https://va.vercel-scripts.com https://ep1.adtrafficquality.google https://ep2.adtrafficquality.google;
-  frame-src https://googleads.g.doubleclick.net https://tpc.googlesyndication.com https://www.google.com https://pagead2.googlesyndication.com;
-  connect-src 'self' https://pagead2.googlesyndication.com https://securepubads.g.doubleclick.net https://ep1.adtrafficquality.google https://ep2.adtrafficquality.google https://va.vercel-scripts.com https://vitals.vercel-insights.com;
-  img-src 'self' data: https://pagead2.googlesyndication.com https://www.gstatic.com https://opengraph.githubassets.com;
-  style-src 'self' 'unsafe-inline';
-  font-src 'self' data:;
-`
 
 module.exports = withNextra({
    redirects: function () {
@@ -32,20 +23,7 @@ module.exports = withNextra({
           destination: '/docs',
           permanent: true,
         }]
-   },
-   async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: cspHeader.replace(/\n/g, ' ').trim(),
-          },
-        ],
-      },
-    ]
-  },
+   }
 })
 
 
